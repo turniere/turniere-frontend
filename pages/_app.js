5f1a8877dfd273d448f78b979529d2606be2171c
JP1998 <littlebigjonny06@gmail.com>
2019-04-09 09:55:45 +0200

    Fix a bug causing the server to always create an error

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { App, Container } from 'next/app';
+import App, { Container } from 'next/app';
 import React              from 'react';
 import { Provider }       from 'react-redux';
 import Notifications      from 'react-notify-toast';
