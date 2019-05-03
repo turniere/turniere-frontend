74a1052e25a822187f9819a065fd7670869abd47
Jonny <littlebigjonny06@gmail.com>
2019-05-03 13:13:56 +0200

    Change for-loop in findTeam to make it more readable

--- js/utils/findTeam.js
+++ js/utils/findTeam.js
@@ -1,8 +1,8 @@
 
 export function findTeam(teams, id) {
-    for(let i = 0; i < teams.length; i++) {
-        if(teams[i].id === id) {
-            return teams[i];
+    for(var team of teams) {
+        if(team.id === id) {
+            return team;
         }
     }
     return null;
