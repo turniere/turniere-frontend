91f7063887e90336f383470761da650d7996f397
Felix Hamme <gitlab@fj.hamme.net>
2019-05-08 13:31:50 +0200

    Rename environment variable API_URL to TURNIERE_API_URL

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@
 const withCSS = require('@zeit/next-css');
 module.exports = withCSS();
 module.exports.publicRuntimeConfig = {
-    api_url: process.env.API_URL
+    api_url: process.env.TURNIERE_API_URL
 };
